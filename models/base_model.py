fd073050b5aa6adae082033a65d43ecddfdc14e3
root <tedygreat@gmail.com>
2023-07-28 19:13:04 +0000

    task

--- models/base_model.py
+++ models/base_model.py
@@ -12,19 +12,17 @@ from sqlalchemy.ext.declarative import declarative_base
 import uuid
 
 time = "%Y-%m-%dT%H:%M:%S.%f"
-
 if models.storage_t == "db":
     Base = declarative_base()
 else:
     Base = object
 
-
 class BaseModel:
     """The BaseModel class from which future classes will be derived"""
     if models.storage_t == "db":
-        id = Column(String(60), primary_key=True)
-        created_at = Column(DateTime, default=datetime.utcnow)
-        updated_at = Column(DateTime, default=datetime.utcnow)
+	    id = Column(String(60), primary_key=True)
+	    created_at = Column(DateTime, default=datetime.utcnow)
+	    updated_at = Column(DateTime, default=datetime.utcnow)
 
     def __init__(self, *args, **kwargs):
         """Initialization of the base model"""
