ef5032d20b76d50dd01ccbb103d52db719885cff
root <tedygreat@gmail.com>
2023-08-01 17:52:51 +0000

    task test

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""
-    app for regiisterinthe g blueprint and starting flask
+""" app for regiisterinthe g blueprint and starting flask
 """
 from flask import Flask, make_response, jsonify
 from flask_cors import CORS
